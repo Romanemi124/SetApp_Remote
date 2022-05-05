b45e5d2a8b18262ec642025f5fa752854413790e
Romanemi124 <emiliojromanrosales@gmail.com>
2022-05-05 13:38:19 +0200

    Commit 12.3:

---
+++ SetApp/Core/Publicaciones/Vista/EditPubliView.swift
@@ -0,0 +1,20 @@
+//
+//  EditPubliView.swift
+//  SetApp
+//
+//  Created by Emilio Roman on 5/5/22.
+//
+
+import SwiftUI
+
+struct EditPubliView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditPubliView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditPubliView()
+    }
+}
